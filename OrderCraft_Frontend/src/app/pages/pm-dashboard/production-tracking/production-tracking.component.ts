99b90911e7dc1a59648c98c52911b17a973f63e5
FIXED-TERM Rai Sarika (BGSW/PJ-ETA-H) <rsi1hyd@bosch.com>
2025-09-25 14:15:19 +0530

    Production Tracking update

---
+++ OrderCraft_Frontend/src/app/pages/pm-dashboard/production-tracking/production-tracking.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-production-tracking',
+  standalone: true,
+  imports: [],
+  templateUrl: './production-tracking.component.html',
+  styleUrl: './production-tracking.component.css'
+})
+export class ProductionTrackingComponent {
+
+}
