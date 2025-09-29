2c0d64eab18b3ac16a7e2b352c797c8a59e9ac90
FIXED-TERM Pranav M V (BGSW/PJ-ETA-H) <pmr1hyd@bosch.com>
2025-09-29 14:16:15 +0530

    filters for reports added

--- OrderCraft_Frontend/src/app/pages/pm-dashboard/pm-reports/pm-reports.component.ts
+++ OrderCraft_Frontend/src/app/pages/pm-dashboard/pm-reports/pm-reports.component.ts
@@ -27,6 +27,8 @@ export class ProductionReportsComponent {
   pageSize = 10;
   currentPage = 1;
 
+  //filters added
+
   constructor(private fb: FormBuilder, private inventoryService: InventoryService) {
     this.reportForm = this.fb.group({
       categoryId: [''],
